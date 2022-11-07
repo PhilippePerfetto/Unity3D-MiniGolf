33ace5a30354c382c2855efd0a071a0d2035eccd
Philippe <philippe.perfetto@hotmail.fr>
2022-11-07 18:43:19 +0100

    Dynamic trees and scene management

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,10 +1,20 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Ball : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        
-        
+        if (other.gameObject.tag == "fall")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        else if (other.gameObject.tag == "fin")
+        {
+            int nextLevel = SceneManager.GetActiveScene().buildIndex + 1;
+            if (nextLevel >= SceneManager.sceneCount)
+                nextLevel = 0;
+            SceneManager.LoadScene(nextLevel);
+        }
     }
 }
