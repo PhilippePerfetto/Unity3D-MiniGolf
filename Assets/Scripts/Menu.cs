73f324202671fe2a78d6a9f14f916b2df702b34d
Philippe <philippe.perfetto@hotmail.fr>
2022-11-13 12:11:38 +0100

    Refactoring-Ground common to all levels and tree prefabs replaced by flowers (to change dynamically)

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,11 +1,27 @@
 using System;
-using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    public GameObject prefabButton;
+    public GameObject flower;
+
+    private void Start()
+    {
+        if (SceneManager.GetActiveScene().name == "Menu")
+        {
+            print("removing GOs");
+            var gos = GameObject.FindGameObjectsWithTag("tree");
+
+            foreach (var go in gos)
+            {
+                var pos = go.transform.position + new Vector3(0, 0.4f, 0);
+                var newGo = Instantiate(flower, pos, Quaternion.identity, gameObject.transform.parent);
+                newGo.AddComponent(Type.GetType("DontDestroyOnLoad"));
+                Destroy(go);
+            }
+        }
+    }
 
     public void LaunchGame()
     {
