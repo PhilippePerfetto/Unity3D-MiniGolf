25a5e2e1c2029cfcb4d824c5baae34e614126ce2
Philippe <philippe.perfetto@hotmail.fr>
2022-11-15 19:26:02 +0100

    4 profiles with distinct decorations now

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -7,31 +7,54 @@ using UnityEngine.UI;
 public class Menu : MonoBehaviour
 {
     public Material[] mat; // liste des ciels
-    public GameObject flower;
+
+    // Ground decorations
+    public GameObject[] prefabs;
+    public Vector3[] moves = new Vector3[10];
+    public Vector3[] rotat = new Vector3[10];
 
     private void Start()
     {
         if (SceneManager.GetActiveScene().name == "Menu") // sécurité, mais inutile
         {
-            UpdateProfileButtons((int)GameManager.Instance.Profile);
-            UpdateLevelButtons();
-
-            ReplaceTrees();
+            ChangeProfile((int)GameManager.Instance.Profile);
         }
     }
 
-    private void ReplaceTrees()
+    private void UpdateGroundDecoration()
     {
-        var gos = GameObject.FindGameObjectsWithTag("tree");
+        var gos = GameObject.FindGameObjectsWithTag("groundDecoration");
         int name = 1;
+        bool randomModel = false;
+        int indexRandomized;
+
+        var index = (int)GameManager.Instance.Profile * 2;
 
         foreach (var go in gos)
         {
-            var pos = go.transform.position + new Vector3(0, 0.4f, 0);
-            var newGo = Instantiate(flower, pos, Quaternion.identity);
+            indexRandomized = randomModel ? index : index + 1;
+            var pos = go.transform.position + moves[indexRandomized];
+
+            if (moves[indexRandomized].z >= 49.0f)
+            {
+                print("trasnform:" + go.transform.position);
+                print("     moves:" + moves[indexRandomized]);
+                print("     pos:" + pos);
+            }
+
+            var newGo = Instantiate(prefabs[indexRandomized], pos, Quaternion.identity);
+            newGo.transform.Rotate(rotat[indexRandomized]);
+            // newGo.transform.position = pos; // useless
+
+            if (moves[indexRandomized].z >= 49.0f)
+                print("         final transform:" + newGo.transform.position);
+
             newGo.AddComponent(Type.GetType("DontDestroyOnLoad")); // work only on root objects !
             newGo.name = name.ToString();
+            newGo.tag = go.tag;
             name++;
+            randomModel = !randomModel;
+
             Destroy(go);
         }
     }
@@ -46,6 +69,7 @@ public class Menu : MonoBehaviour
         GameManager.Instance.Profile = (GameManager.Profiles)profile;
         UpdateProfileButtons(profile);
         UpdateLevelButtons();
+        UpdateGroundDecoration();
 
         RenderSettings.skybox = mat[profile];
     }
